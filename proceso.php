06c9cb3e928b7369604a712c40ab6c4ca6d6fa33
Ángela Olmo Martinez <aolmmar159@g.educaand.es>
2021-10-06 09:51:10 +0200

    Versión 2

--- proceso.php
+++ proceso.php
@@ -1,7 +1,7 @@
 <?php 
 
 function validar(){
-    if (isset($_POST["suma"])){
+    if (isset($_POST["suma"]) || ($_POST["resta"]) || ($_POST["multi"]) || ($_POST["division"])){
         if (is_numeric($_POST['n1']) && is_numeric($_POST['n2'])){
             return true; 
         }
@@ -10,15 +10,37 @@ function validar(){
 
 }
 
-if (validar()){
-    $n1=$_POST['n1'];
-    $n2= $_POST['n2'];
-    $suma= $n1 + $n2;
-    print_r($suma);
+switch(validar()){
+    case 'suma':
+        $n1=$_POST['n1'];
+        $n2= $_POST['n2'];
+        $suma= $n1 + $n2;
+        print_r($suma);
+        break;
+    case 'resta':
+        $n1=$_POST['n1'];
+        $n2= $_POST['n2'];
+        $resta= $n1 - $n2;
+        print_r($resta);
+        break;
+    case 'multi':
+        $n1=$_POST['n1'];
+        $n2= $_POST['n2'];
+        $multi= $n1 * $n2;
+        print_r($multi);
+        break;
+    case 'division':
+        $n1=$_POST['n1'];
+        $n2= $_POST['n2'];
+        $division= $n1 / $n2;
+        print_r($division);
+        break;            
 }
 
-else{
+
+/*else{
     header("Location:form.html");
 }
+*/
 ?>
 
